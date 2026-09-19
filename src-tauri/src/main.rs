75d4fd9267f0a268b9cc55f3f2a6bb83075bed05
Sulaiman Waziri <SulamiWizard@gmail.com>
2026-09-18 22:00:38 -0700

    fix wayland env var

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    unsafe {
+        // temporary fix for: https://github.com/tauri-apps/tauri/issues/5143
+        std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+    }
     rinsham_lib::run()
 }
